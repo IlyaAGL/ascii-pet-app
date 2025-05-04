3e975a4daa88a403bd1221f2a6b651f5875eadd8
IlyaAGL <welcomenoinside@gmail.com>
2025-05-04 14:26:23 +0300

    chore: add logs

--- backend/internal/infrastructure/ascii_repository.go
+++ backend/internal/infrastructure/ascii_repository.go
@@ -18,7 +18,7 @@ type FileAsciiRepository struct{}
 
 func NewAsciiRepository() *FileAsciiRepository {
 	if err := os.MkdirAll(dataDir, 0755); err != nil { // Убедимся, что папка существует
-		logger.Log.Info("The folder ./data already exists", "ERROR", err.Error())
+		logger.Log.Error("The folder ./data already exists", "ERROR", err.Error())
 
 		return nil
 	}
@@ -29,10 +29,14 @@ func NewAsciiRepository() *FileAsciiRepository {
 func (r *FileAsciiRepository) GetAscii() (entities.Ascii, error) {
 	files, err := filepath.Glob(filepath.Join(dataDir, "*.json")) // Получаем набор паттернов файлов, которые имеют расширение .json
 	if err != nil {
+		logger.Log.Error("Something went wrong during retrieving json files", "ERROR", err.Error())
+
 		return entities.Ascii{}, err
 	}
 
 	if len(files) == 0 {
+		logger.Log.Info("No ASCII files found")
+
 		return entities.Ascii{}, errors.New("no ASCII files found")
 	}
 
@@ -40,15 +44,20 @@ func (r *FileAsciiRepository) GetAscii() (entities.Ascii, error) {
 
 	buf, err := os.ReadFile(randomFile)
 	if err != nil {
+		logger.Log.Error("Couldn't read the chosen file", "ERROR", err.Error())
+
 		return entities.Ascii{}, err
 	}
 
 	var a entities.Ascii
 	if err := json.Unmarshal(buf, &a); err != nil {
+		logger.Log.Error("Couldn't parse the chosen file", "ERROR", err.Error())
+
 		return entities.Ascii{}, err
 	}
 
 	logger.Log.Info("Loaded ASCII from file", "file", randomFile, "description", a.Description)
+
 	return a, nil
 }
 
@@ -60,29 +69,39 @@ func (r *FileAsciiRepository) UploadAscii(ascii entities.Ascii) error {
 
 	buf, err := json.MarshalIndent(ascii, "", "  ")
 	if err != nil {
+		logger.Log.Error("Couldn't parse the ASCII to JSON", "ERROR", err.Error())
+
 		return err
 	}
 
 	if err := os.WriteFile(fullPath, buf, 0644); err != nil {
+		logger.Log.Error("Something went wrong during writing to the file", "ERROR", err.Error())
+
 		return err
 	}
 
 	logger.Log.Info("Saved ASCII to file", "file", fullPath, "description", ascii.Description)
+
 	return nil
 }
 
 func (r *FileAsciiRepository) DeleteAscii() error {
 	files, err := filepath.Glob(filepath.Join(dataDir, "*.json")) // Получаем набор паттернов файлов, которые имеют расширение .json
 	if err != nil {
+		logger.Log.Error("Something went wrong during retrieving json files", "ERROR", err.Error())
+
 		return err
 	}
 
 	for _, f := range files {
 		if err := os.Remove(f); err != nil {
+			logger.Log.Error("Something went wrong during deleting json file", "ERROR", err.Error())
+
 			return err
 		}
 	}
 
 	logger.Log.Info("Deleted all ASCII files")
+
 	return nil
 }
